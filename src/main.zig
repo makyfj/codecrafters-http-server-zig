85614edc6a7b85a8761fa5b75daa0aa707a45ef2
Franklin <franklinjaradev@gmail.com>
2024-09-02 02:23:55 -0400

    pass stage

--- src/main.zig
+++ src/main.zig
@@ -16,6 +16,29 @@ pub fn main() !void {
 
     const client = try listener.accept();
 
+    // Create a buffer to read the client's request
+    var buffer: [1024]u8 = undefined;
+
+    const bytes_read = try client.stream.read(&buffer);
+    const request = buffer[0..bytes_read];
+
+    // Parse the request
+    var lines = std.mem.splitSequence(u8, request, "\r\n");
+
+    const first_line = lines.next() orelse return error.InvalidRequest;
+    var parts = std.mem.split(u8, first_line, " ");
+    _ = parts.next() orelse return error.InvalidRequest;
+    const path = parts.next() orelse return error.InvalidRequest;
+    _ = parts.next() orelse return error.InvalidRequest;
+
+    if (std.mem.eql(u8, path, "/abcdefg")) {
+        try client.stream.writeAll("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+
+    if (std.mem.eql(u8, path, "/")) {
+        try client.stream.writeAll("HTTP/1.1 200 OK\r\n\r\n");
+    }
+
     // Send a response to the client
-    try client.stream.writeAll("HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, world!");
+    // try client.stream.writeAll("HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, world!");
 }
